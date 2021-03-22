0a1f9c26e5999229f8e5a9520b4a724483e7fcac
parlaga <parlaga@mapfre.net>
2021-03-22 21:09:36 +0100

    add build and push, change diferent properties

--- toposervice/src/main/java/org/mastercloudapps/toposervice/ToposerviceApplication.java
+++ toposervice/src/main/java/org/mastercloudapps/toposervice/ToposerviceApplication.java
@@ -1,11 +1,21 @@
 package org.mastercloudapps.toposervice;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.policy.AlwaysRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @SpringBootApplication
 public class ToposerviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToposerviceApplication.class, args);
+		//Retry db connection
+		RetryTemplate template = new RetryTemplate();
+		AlwaysRetryPolicy policy = new AlwaysRetryPolicy();
+		template.setRetryPolicy(policy);
+		template.execute(context -> {
+			SpringApplication.run(ToposerviceApplication.class, args);
+			return true;
+		});
 	}
 }
